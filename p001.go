c12189654d10718a0308558fa7edbb2a0acfb76c
Matt Donahoe <matt.donahoe@gmail.com>
2021-01-03 01:36:21 +0000

    update p001 fmt

--- p001.go
+++ p001.go
@@ -1,11 +1,11 @@
-package main
+package p001
 
 import "fmt"
 
 func main() {
 	total := 0
-	for i:=0; i<1000; i++ {
-		if i % 3 == 0 || i % 5 == 0 {
+	for i := 0; i < 1000; i++ {
+		if i%3 == 0 || i%5 == 0 {
 			total += i
 		}
 	}
