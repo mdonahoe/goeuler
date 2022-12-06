783405937d111d25e8d150ed78d5572cd78a8e9e
Matt Donahoe <matt.donahoe@gmail.com>
2022-12-06 04:01:46 +0000

    wip 98

--- problems/p098.go
+++ problems/p098.go
@@ -1,5 +1,16 @@
 package main
 
+/*
+By replacing each of the letters in the word CARE with 1, 2, 9, and 6 respectively, we form a square number: 1296 = 36^2.
+What is remarkable is that, by using the same digital substitutions, the anagram, RACE, also forms a square number: 9216 = 96^2.
+We shall call CARE (and RACE) a square anagram word pair and specify further that leading zeroes are not permitted,
+neither may a different letter have the same digital value as another letter.
+
+Using words.txt (right click and 'Save Link/Target As...'), a 16K text file containing nearly two-thousand common English words,
+find all the square anagram word pairs (a palindromic word is NOT considered to be an anagram of itself).
+
+What is the largest square number formed by any member of such a pair?
+*/
 
 import (
 	"fmt"
@@ -88,11 +99,12 @@ func main() {
     sort.Slice(keysToKeep, func(i, j int) bool {
         return len(keysToKeep[i]) > len(keysToKeep[j])
     })
+    fmt.Println(keysToKeep)
 
     // for each key, assign a value for each number
     // we need to permute all the numbers 0-9
-    ps := Permutations([]int{9,8,7,6,5,4,3,2,1,0}, 9)
-    fmt.Println(ps)
+    // ps := Permutations([]int{9,8,7,6,5,4,3,2,1,0}, 9)
+    // fmt.Println(ps)
 
 
     /*
